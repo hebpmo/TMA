9e54060d2bcb05e35300a09a28696acafd2ad482
zengbin93 <zeng_bin8888@163.com>
2018-08-15 20:55:32 +0800

    modify

--- tma/analyst/kline.py
+++ tma/analyst/kline.py
@@ -5,13 +5,11 @@ analyst.kline - K线分析
 ====================================================================
 """
 
-
 from tma.collector.ts import get_klines
 
-
-kls = get_klines('600165', freq='D', start_date='2018-01-01')
-
-
+kls1 = get_klines('600165', freq='D', start_date='2018-01-01')
+kls2 = get_klines('600682', freq='D', start_date='2018-01-01')
 
 
-
+class KlineSimilarity(object):
+    pass
