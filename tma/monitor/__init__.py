0b765c69c2f90641139b3fca9e8b4ec8830ecbf0
zengbin93 <zeng_bin8888@163.com>
2018-08-19 16:35:49 +0800

    modify

--- tma/monitor/__init__.py
+++ tma/monitor/__init__.py
@@ -1,4 +1,4 @@
 # -*- coding: UTF-8 -*-
 
-from .single import sm_limit
-from .market import get_market_status
+from .single import sm_limit, get_shares_status
+from .market import get_market_status, get_indices_status
